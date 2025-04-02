430a00e1eb85583c4cba8ad5a136b6d951c12e95
Kein Moon <successhelper2000@gmail.com>
2025-04-02 15:09:42 +0900

    2

--- lib/data_source/impl/todo_data_source_impl.dart
+++ lib/data_source/impl/todo_data_source_impl.dart
@@ -1,16 +1,29 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:todo_app/data_source/local/todo_data_source.dart';
 
 class TodoDataSourceImpl implements TodoDataSource {
+
+  final String filePath;
+
+  TodoDataSourceImpl({required this.filePath});
+
   @override
   Future<List<Map<String, dynamic>>> readTodos() {
-    // TODO: implement readTodos
-    throw UnimplementedError();
+
   }
 
   @override
-  Future<void> writeTodos(List<Map<String, dynamic>> todos) {
-    // TODO: implement writeTodos
-    throw UnimplementedError();
+  Future<void> writeTodos(List<Map<String, dynamic>> todos) async {
+
+    final File file = File(filePath);
+    final jsonString = jsonEncode(todos);
+    await file.writeAsString(jsonString);
+
+
+
+
   }
   
   
